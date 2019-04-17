9536ea89e3975c5cb9d88a396b3c2ddd2a8b6ff3
xiayinchang <xiayinchang@gmail.com>
2019-04-17 15:03:24 +0800

    feat: add min_age and include

--- init.sh
+++ init.sh
@@ -2,5 +2,14 @@
 
 rclone config create umstor s3 access_key_id ${UMSTOR_ACCESS_KEY} secret_access_key ${UMSTOR_SECRET_KEY} provider Ceph endpoint ${UMSTOR_ENDPOINT}
 rclone config create alioss s3 access_key_id ${ALIOSS_ACCESS_KEY} secret_access_key ${ALIOSS_SECRET_KEY} provider Alibaba endpoint ${ALIOSS_ENDPOINT}
-rclone sync alioss:/${ALIOSS_BUCKET} umstor:/${UMSTOR_BUCKET} --progress
-rclone check alioss:/${ALIOSS_BUCKET} umstor:/${UMSTOR_BUCKET}
+
+if [ -z "$FILE_PREFIX" ]; then
+    FILE_PREFIX="*"
+fi
+
+if [ -z "$MIN_AGE" ]; then  
+    MIN_AGE="1ms"
+fi
+
+rclone sync alioss://${ALIOSS_BUCKET} umstor://${UMSTOR_BUCKET} --include=${FILE_PREFIX} --min-age=${MIN_AGE} --progress
+rclone check alioss://${ALIOSS_BUCKET} umstor://${UMSTOR_BUCKET}
